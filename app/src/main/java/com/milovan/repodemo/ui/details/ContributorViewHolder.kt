c879e50fcbbaf6b2fd41d42346b91567b651fccc
milovan <stamatowsky@gmail.com>
2025-03-03 01:00:16 +0100

    Prikazi avatare kontributora

--- app/src/main/java/com/milovan/repodemo/ui/details/ContributorViewHolder.kt
+++ app/src/main/java/com/milovan/repodemo/ui/details/ContributorViewHolder.kt
@@ -1,6 +1,7 @@
 package com.milovan.repodemo.ui.details
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.milovan.repodemo.R
 import com.milovan.repodemo.databinding.ViewholderContributorBinding
 
@@ -18,6 +19,10 @@ class ContributorViewHolder(
     fun bind(item: ContributorUi) {
         binding.apply {
             textName.text = item.contributor.login
+            Glide.with(binding.root.context)
+                .load(item.contributor.avatarUrl)
+                .into(binding.imgAvatar)
+
             val imgResource = if (item.isFavorite) {
                 R.drawable.ic_favorite_24dp_filled
             } else {
